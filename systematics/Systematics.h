96825e5ab7e640b4f5f758f86c87b47b77a4faa6
cpernas <cpernas@wm.edu>
2025-10-10 05:24:48 -0500

    Adding some additional systematic universes to match the systematics from previous nu_e analyses. Still some things missing, I should confirm those I've added are working as I expect

--- systematics/Systematics.h
+++ systematics/Systematics.h
@@ -10,12 +10,99 @@
 #include "PlotUtils/GenieSystematics.h"
 #include "PlotUtils/MinosEfficiencySystematics.h"
 #include "PlotUtils/MuonSystematics.h"
+//#include "PlotUtils/ElectronSystematics.h"
 #include "PlotUtils/MnvTuneSystematics.h"
 #include "PlotUtils/MuonResolutionSystematics.h"
 #include "PlotUtils/AngleSystematics.h"
+#include "PlotUtils/ResponseSystematics.h"
+#include "PlotUtils/MnvHadronReweight.h"
+#include "PlotUtils/GeantHadronSystematics.h"
+#include "PlotUtils/TargetMassSystematics.h"
 
 typedef std::map<std::string, std::vector<CVUniverse*>> UniverseMap;
+UniverseMap GetNueTKISystematics(PlotUtils::ChainWrapper* chain)
+{
+  // return map
+  UniverseMap error_bands;
 
+  // CV
+  error_bands[std::string("cv")].push_back(new CVUniverse(chain));
+
+  //========================================================================
+  // FLUX
+  //========================================================================
+  UniverseMap bands_flux = PlotUtils::GetFluxSystematicsMap<CVUniverse>(chain, CVUniverse::GetNFluxUniverses());
+  error_bands.insert(bands_flux.begin(), bands_flux.end());
+
+  //========================================================================
+  // GENIE
+  //========================================================================
+  // Standard
+  UniverseMap bands_genie = PlotUtils::GetGenieSystematicsMap<CVUniverse>(chain); //PlotUtils::GetStandardGenieSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_genie.begin(), bands_genie.end());
+
+  //Ok sweet this works, although I don't actually need it. 
+  //error_bands["NormCCRes"].push_back(new PlotUtils::GenieNormCCResUniverse<CVUniverse>(chain, -1.));
+  //error_bands["NormCCRes"].push_back(new PlotUtils::GenieNormCCResUniverse<CVUniverse>(chain, 1.));
+  
+  //========================================================================
+  // MnvTunes
+  //========================================================================
+  // RPA
+  UniverseMap bands_rpa = PlotUtils::GetRPASystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_rpa.begin(), bands_rpa.end());
+
+  // 2P2H
+  UniverseMap bands_2p2h = PlotUtils::Get2p2hSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_2p2h.begin(), bands_2p2h.end());
+
+  //========================================================================
+  // Muons
+  //========================================================================
+  // Muon reco in MINERvA -- Catchall systematic for pmu reco in minerva.
+  // Lateral-only. Shifts pmu.
+  UniverseMap bands_muon_minerva = PlotUtils::GetMinervaMuonSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_muon_minerva.begin(), bands_muon_minerva.end());
+
+  UniverseMap bands_electron = PlotUtils::GetMinervaMuonSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_muon_minerva.begin(), bands_muon_minerva.end());
+
+  // Muons in MINOS -- Catchall systematic for wiggle solution -- correlates
+  // flux universes and minos muon momentum reco.
+  // Lateral AND Vertical systematic. Shifts Pmu and GetFluxAndCVUniverse.
+  // Expect a non-zero systematic even when no pmu involved.
+  //
+  //Carlos - Can I remove this??? Apparently it has flux implications so I'm not so sure
+  UniverseMap bands_muon_minos = PlotUtils::GetMinosMuonSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_muon_minos.begin(), bands_muon_minos.end());
+
+  UniverseMap bands_muon_resolution = PlotUtils::GetMuonResolutionSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_muon_resolution.begin(), bands_muon_resolution.end());
+
+  UniverseMap bands_geant = PlotUtils::GetGeantHadronSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_geant.begin(), bands_geant.end());
+
+  // Beam angle
+  UniverseMap bands_angle = PlotUtils::GetAngleSystematicsMap<CVUniverse>(chain);
+  error_bands.insert(bands_angle.begin(), bands_angle.end());
+
+  //========================================================================
+  // Particle Response Systematics
+  //========================================================================
+  //Using Anezka's reworked recoil systematics after p4 tuples
+  const bool use_ID = true;
+  const bool use_OD = true;
+  std::string name_tag = "allNonMuonClusters";
+  const bool use_neutron = false;
+  const bool use_new = false;
+  const bool use_proton = true;
+  UniverseMap bands_response = PlotUtils::GetResponseSystematicsMap<CVUniverse>(chain, use_ID, use_OD, name_tag, use_neutron, use_new, use_proton);
+  error_bands.insert(bands_response.begin(), bands_response.end());
+
+  return error_bands;
+}
+
+//Legacy, this was in the tutorial. 
 UniverseMap GetStandardSystematics(PlotUtils::ChainWrapper* chain)
 {
   // return map
@@ -38,7 +125,7 @@ UniverseMap GetStandardSystematics(PlotUtils::ChainWrapper* chain)
   UniverseMap bands_genie =
       PlotUtils::GetGenieSystematicsMap<CVUniverse>(chain); //PlotUtils::GetStandardGenieSystematicsMap<CVUniverse>(chain);
   error_bands.insert(bands_genie.begin(), bands_genie.end());
-
+  
   //========================================================================
   // MnvTunes
   //========================================================================
@@ -90,5 +177,6 @@ UniverseMap GetStandardSystematics(PlotUtils::ChainWrapper* chain)
 
   return error_bands;
 }
+
 
 #endif  // Systematics_h
