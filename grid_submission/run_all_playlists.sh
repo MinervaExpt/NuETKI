01ae46b927cda9435af5ce233eda12b1d1da34d7
cpernas <cpernas@wm.edu>
2026-03-13 10:21:25 -0500

    Updating my simple script to run multiple jobs to a loop, also adding the config argument. Also added a script to run a set of N-1 jobs using their own dedicated configs

--- grid_submission/run_all_playlists.sh
+++ grid_submission/run_all_playlists.sh
@@ -1,12 +1,56 @@
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1A --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1A
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1B --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1B
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1C --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1C
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1D --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1D
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1E --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1E
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1F --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1F
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1G --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1G
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1L --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1L
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1M --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1M
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1N --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1N
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1O --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1O
-python SubmitJobsToGrid.py --stage eventLoop --playlist minervame1P --basedir /exp/minerva/app/users/cpernas/MAT_AL9/ --outdir /pnfs/minerva/persistent/users/cpernas/me1P
+#!/bin/bash
+
+# ---- configuration ----
+TODAY=$(date +"%b_%d")
+#TODAY="Feb_25"
+BASEDIR="/exp/minerva/app/users/cpernas/MAT_AL9/"
+OUTBASE="/pnfs/minerva/scratch/users/cpernas/${TODAY}"
+
+# output file lists (start clean)
+CUT_LIST="CutSummary_files.txt"
+DATA_LIST="DataHists_${TODAY}.txt"
+MC_LIST="MCHists_${TODAY}.txt"
+
+> "$CUT_LIST"
+> "$DATA_LIST"
+> "$MC_LIST"
+
+# ---- playlists ----
+playlists=(
+  minervame1A
+  minervame1B
+  minervame1C
+  minervame1D
+  minervame1E
+  minervame1F
+  minervame1G
+  minervame1L
+  minervame1M
+  minervame1N
+  minervame1O
+  minervame1P
+)
+
+# ---- submit jobs + record outputs ----
+for pl in "${playlists[@]}"; do
+  letter=$(echo "$pl" | sed -E 's/minervame1([A-Z])_.*/\1/')
+  outdir="${OUTBASE}/${letter}"
+
+  # submit job
+  python SubmitJobsToGrid.py \
+    --stage eventLoop \
+    --playlist "$pl" \
+    --basedir "$BASEDIR" \
+    --outdir "$outdir" \
+    --config "${BASEDIR}NuE_TKI/config/analysis.yaml"
+
+  # record expected outputs
+  echo "${outdir}/CutSummary.txt"        >> "$CUT_LIST"
+  echo "${outdir}/Data_${TODAY}.root"    >> "$DATA_LIST"
+  echo "${outdir}/MC_${TODAY}.root"      >> "$MC_LIST"
+done
+
+#copy the lists of output files to the base directory, helps to keep track of them
+cp "$CUT_LIST" "${OUTBASE}"
+cp "$DATA_LIST" "${OUTBASE}"
+cp "$MC_LIST" "${OUTBASE}"
