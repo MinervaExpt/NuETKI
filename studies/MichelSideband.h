2033ee8e1ded32c05de962869a86147a5dc73beb
cpernas <cpernas@wm.edu>
2026-05-12 13:52:47 -0500

    With the change to add additional breakdowns to Variable.h, need to match the logic of runEventLoop for identifying bkg and sig category IDs

--- studies/MichelSideband.h
+++ studies/MichelSideband.h
@@ -82,37 +82,50 @@ class MichelSideband: public Study
     using Hist = PlotUtils::HistWrapper<CVUniverse>;
 
       void fillSelectedMC(const CVUniverse& univ, const CCNuEEvent& evt, const double weight) {
-	//std::cout << "Entering MichelSideband::fillSelectedMC" << std::endl;
-	//std::cout << "improved_nmichel = " << univ.GetImprovedNMichel() << std::endl;      
-	//std::cout << "N Iso Clusters = " << univ.GetNIsoBlobs() << std::endl;      
-
       //if (univ.GetImprovedNMichel() > 0 && univ.GetNIsoBlobs() < 2){
       if (univ.GetImprovedNMichel() > 0){
 
-	//g_OutputTreeManager.Fill("Michel_Sideband", evt.entryNumber); //add this event to the michel sb tree of my output tree, selectionCategory is already set earlier
-
 	for (auto& var: fVars) var->selectedMCReco->FillUniverse(&univ, var->GetRecoValue(univ), weight);
-	//const bool isSignal = false; //figure out how to check this later, I don't have michelcuts to do it for me :/
 
 	const auto vertex = univ.GetTrueVertex();
         const bool inApothem = (fabs(vertex.y()) < (-1./sqrt(3.))*fabs(vertex.x()) + 2.*850/sqrt(3.)) && (fabs(vertex.x()) < 850);
 
 	//This checks all of my signal definition requirements (tbh I can also use my selectioncategory from g_outputManager for this, plus it uses michelcuts.issignal
 	if (abs(univ.GetTruthNuPDG())==12 && univ.GetCurrent()==1 && univ.GetElectronEnergyTrue()>=2.5 && univ.GetHasSignalFSProton()==1 && univ.GetHasFSMeson()==0 && univ.GetHasFSPhoton()==0 && univ.GetTrueVertex().z() >= 5980 && univ.GetTrueVertex().z() <= 8422 && inApothem) {
-	//if (isSignal) { 
+
+	  //determine signal category by interaction type
+	  int sig_ID = -1; //sig_ID == -1 -> NuE CCQELike other, this is DIS & a bit of coherent
+	  if (univ.GetInteractionType()==1){ sig_ID = 0; } //NuE CCQELike QE
+	  else if (univ.GetInteractionType()==2){ sig_ID = 1; } //NuE CCQELike RES
+	  else if (univ.GetInteractionType()==8){ sig_ID = 2; } //NuE CCQELike 2p2h
+
+	  //fill vars
 	  for (auto& var: fVars){
 	    var->efficiencyNumerator->FillUniverse(&univ, var->GetTrueValue(univ), weight);
             var->migration->FillUniverse(&univ, var->GetRecoValue(univ), var->GetTrueValue(univ), weight);
             var->selectedSignalReco->FillUniverse(&univ, var->GetRecoValue(univ), weight);
-	  }
+	    (*var->m_signalBreakdown)[sig_ID].FillUniverse(&univ, var->GetRecoValue(univ), weight);	  }
 	  //for(auto& var: fVars2D) var->efficiencyNumerator->FillUniverse(&univ, var->GetTrueValueX(univ) var->GetTrueValueY(univ), weight);
   	}
 	else{
-	  int bkgd_ID = -1;
-          if (abs(univ.GetTruthNuPDG())==12 && univ.GetCurrent()==1 && univ.GetHasFSMeson()) { bkgd_ID = 0; }
-          else if (abs(univ.GetTruthNuPDG())==12 && univ.GetCurrent()==1) { bkgd_ID = 1; }
-          else if (univ.GetCurrent()==2 && univ.GetHasFSPi0()==1) { bkgd_ID = 2; }
-          else if (abs(univ.GetTruthNuPDG())==14 && univ.GetCurrent()==1 && univ.GetHasFSPi0()==1) { bkgd_ID = 3; }
+	  int bkgd_ID = -1; //this is "other" selectionCategory
+	  
+	  //NueCC with final state mesons (mostly pions)
+	  //if (abs(univ.GetTruthNuPDG())==12 && univ.GetCurrent()==1 && univ.GetHasFSMeson()) { bkgd_ID = 0; }
+	  if (univ.GetTruthNuPDG()==12 && univ.GetCurrent()==1 && univ.GetHasSinglePiPlus()) { bkgd_ID = 0; }
+	  else if (univ.GetTruthNuPDG()==12 && univ.GetCurrent()==1 && univ.GetHasSinglePiMinus()) { bkgd_ID = 1; }
+	  else if (univ.GetTruthNuPDG()==12 && univ.GetCurrent()==1 && univ.GetHasSinglePiZero()) { bkgd_ID = 2; }
+	  else if (univ.GetTruthNuPDG()==12 && univ.GetCurrent()==1 && univ.GetHasMultiplePions()) { bkgd_ID = 3; }
+
+	  //Other NueCC. mostly just consists of completely signal-like events but protons are outside of kinematic reqs. Also wrong includes wrong sign nues
+	  else if (abs(univ.GetTruthNuPDG())==12 && univ.GetCurrent()==1) { bkgd_ID = 4; }
+
+	  //Other NC pi0, all "other" NC i get should have pi0 so if this is small and the "other" selectionCategory is large that's a problem
+	  else if (univ.GetCurrent()==2 && univ.GetHasFSPi0()==1) { bkgd_ID = 5; }
+	  //same logic here with cc numu pi0
+	  else if (abs(univ.GetTruthNuPDG())==14 && univ.GetCurrent()==1 && univ.GetHasFSPi0()==1) { bkgd_ID = 6; }
+
+	  //fill vars
 	  for(auto& var: fVars) (*var->m_backgroundHists)[bkgd_ID].FillUniverse(&univ, var->GetRecoValue(univ), weight);
 	  //for(auto& var: fVars2D) (*var->m_backgroundHists)[bkgd_ID].FillUniverse(&univ, var->GetRecoValueX(univ), var->GetRecoValueY(univ), weight);
 	}
